cc08404e9777b9f24fa19b10d4d4a57ff64db71a
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-08 20:57:17 +0100

    feat: remove imagefrop app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import CompressImage from './components/CompressImage'
 import ConvertText from './components/ConvertText'
 import { compress, decompress } from './utils/rle'
 
@@ -21,16 +20,6 @@ function App() {
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
         <ConvertText title='Compress' convertFunction={compress} convertedTitle='Compress text' />
         <ConvertText title='Decompress' convertFunction={decompress} convertedTitle='Decompress text' />
-      </div>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr',
-          textAlign: 'center',
-          gap: '5rem',
-        }}
-      >
-        <CompressImage />
       </div>
     </div>
   )
