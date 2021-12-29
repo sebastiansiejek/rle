b0b55a9c4af4d2ba3f25646aebd074521cbdd9c1
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-12-29 18:03:12 +0100

    feat: add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Divider, Typography } from '@mui/material'
 import CompressFile from './components/CompressFile'
 import ConvertText from './components/ConvertText'
 import DecompressFile from './components/DecompressFile'
@@ -5,39 +6,54 @@ import { compress, decompress } from './utils/rle'
 
 function App() {
   return (
-    <div
-      style={{
-        display: 'grid',
-        gap: '2rem',
-      }}
-    >
+    <div>
+      <div
+        style={{
+          textAlign: 'center',
+          marginBottom: '1.5rem',
+        }}
+      >
+        <strong>
+          <Typography variant='h1' fontSize={'5rem'} paddingBottom={'1.5rem'}>
+            Run-Length Encoding
+          </Typography>
+        </strong>
+        <Divider light />
+      </div>
       <div
         style={{
           display: 'grid',
-          gridTemplateColumns: '1fr 1fr',
-          textAlign: 'center',
-          gap: '5rem',
+          gap: '2rem',
         }}
       >
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
-        <div>
-          <ConvertText title='Compress' convertFunction={compress} convertedTitle='Compress text' />
-          <div
-            style={{
-              marginTop: '1rem',
-            }}
-          >
-            <CompressFile />
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr',
+            textAlign: 'center',
+            gap: '5rem',
+          }}
+        >
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
+          <div>
+            <ConvertText title='Compress' convertFunction={compress} convertedTitle='Compress text' />
+            <div
+              style={{
+                marginTop: '1rem',
+              }}
+            >
+              <CompressFile />
+            </div>
           </div>
-        </div>
-        <div>
-          <ConvertText title='Decompress' convertFunction={decompress} convertedTitle='Decompress text' />
-          <div
-            style={{
-              marginTop: '1rem',
-            }}
-          >
-            <DecompressFile />
+          <div>
+            <ConvertText title='Decompress' convertFunction={decompress} convertedTitle='Decompress text' />
+            <div
+              style={{
+                marginTop: '1rem',
+              }}
+            >
+              <DecompressFile />
+            </div>
           </div>
         </div>
       </div>
