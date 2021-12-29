f2ebecd15d9c5a718a4ebb6435dc1459e7764ccb
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-12-29 20:00:58 +0100

    fix: support for count more than 9

--- src/utils/rle.ts
+++ src/utils/rle.ts
@@ -13,24 +13,74 @@ function compress(text: string) {
     result += '-' + count + char
   }
 
+  result = result.substring(1)
+
   return result
 }
 
 function decompress(text: string) {
   let result = ''
 
-  for (let i = 0; i < text.length; i++) {
-    let sign = text[i]
+  const findPart = (
+    text: string,
+    startPosition: number = 0
+  ): {
+    count: number
+    char: string | number
+  } => {
+    let count = ''
+    let char = ''
 
-    if (sign === '-' && !Number.isNaN(Number(text[i + 1]))) {
-      let count = parseInt(text[i + 1])
-      let char = text[i + 2]
+    for (let index = startPosition; index < text.length; index++) {
+      char = text[index]
 
-      for (let j = 0; j < count; j++) {
-        result += char
+      if (text[index] !== '-' || index === text.length) {
+        count += char
+      } else {
+        count = count.slice(0, -1)
+
+        return {
+          count: Number(count),
+          char: text[index - 1],
+        }
       }
+    }
 
-      i += 2
+    return {
+      count: Number(count.slice(0, -1)),
+      char,
+    }
+  }
+
+  const concatResult = (char: string | number, count: number) => {
+    let result = ''
+
+    for (let j = 0; j < count; j++) {
+      result += char
+    }
+
+    return result
+  }
+
+  const isSinglePart = !text.includes('-')
+
+  if (isSinglePart) {
+    const { count, char } = findPart(text)
+
+    for (let j = 0; j < count; j++) {
+      result += char
+    }
+  }
+
+  if (!isSinglePart) {
+    const { count, char } = findPart(text, 0)
+    result += concatResult(char, count)
+
+    for (let index = 0; index < text.length; index++) {
+      if (text[index] === '-') {
+        const { count, char } = findPart(text, index + 1)
+        result += concatResult(char, count)
+      }
     }
   }
 
